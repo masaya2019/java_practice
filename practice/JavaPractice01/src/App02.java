d826eb967b03389c517aa9200b377720d7a6adc4
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2021-01-28 01:02:23 +0900

    練習１を追加

---
+++ practice/JavaPractice01/src/App02.java
@@ -0,0 +1,9 @@
+public class App02 {
+  public static void main(String[] args) throws Exception {
+    boolean result = true;
+    char name = '駆';
+    double pi = 3.14;
+    Long number = 314159265853979L;
+    String text = "ミナトの攻撃！敵に１５のダメージを与えた！";
+  }
+}
